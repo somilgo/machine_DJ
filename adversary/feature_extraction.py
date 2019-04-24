e62f75561f354a6509d0eccad50f625c89d8303b
Somil Govani <somilgovani@gmail.com>
2019-04-23 20:06:58 -0400

    exploring the drops

--- adversary/feature_extraction.py
+++ adversary/feature_extraction.py
@@ -6,6 +6,24 @@ STRIDE_SIZE = 10.
 FRAME_SIZE = 25.
 N_MFCC = 16
 
+WINDOW = 3000
+WINDOW_STRIDE = 30
+
+def find_peaks(X, Y, iterations=1):
+	out = list(zip(X, Y))
+	for i in range(iterations):
+		temp_out = []
+		for j in range(len(out)):
+			if (j-1) >= 0 and (j+1) < len(out):
+				if out[j-1][1] < out[j][1] and out[j][1] < out[j+1][1]:
+					temp_out.append(out[j])
+		out = temp_out
+	return out
+
+def window_number_to_sec(win_num, win_len = WINDOW):
+	stamp = win_num + (win_len // 2)
+	return (FRAME_SIZE + STRIDE_SIZE * (stamp - 1)) / 1000.
+
 def get_mfcc_features(song_data, sample_rate, stride_size = STRIDE_SIZE, frame_size = FRAME_SIZE):
 	mfccs = librosa.feature.mfcc(song_data, sample_rate, 
 								 n_mfcc=N_MFCC,
@@ -13,34 +31,52 @@ def get_mfcc_features(song_data, sample_rate, stride_size = STRIDE_SIZE, frame_s
 								 n_fft=int(FRAME_SIZE / 1000. * sample_rate))
 	return mfccs
 
-file = "../wavs/pop/pop.00008.wav"
+# file = "../wavs/pop/pop.00009.wav"
+# data, samplerate = sf.read(file)
+
+# file = "../wavs/rock/rock.00021.wav"
+# data2, samplerate2 = sf.read(file)
+# print(len(data))
+# joined_data = np.concatenate((data[200:200+len(data)//2], data2[len(data2)//2:]))
+# print(np.shape(joined_data))
+
+file = "../kiss.wav"
 data, samplerate = sf.read(file)
-mfcc_fts = get_mfcc_features(data[:len(data)//2], samplerate)
-print(len(mfcc_fts), len(mfcc_fts[0]))
-mfcc_fts = np.asarray(mfcc_fts).T
-print(np.shape(mfcc_fts))
+	
+print(np.shape(data))
+data = data[:, 0]
+mfccs = get_mfcc_features(data, samplerate)
+mfccs = np.asarray(mfccs).T
+print(np.shape(mfccs))
 
 from hmmlearn import hmm
 
-model = hmm.GaussianHMM(n_components=100)
-print(np.shape(mfcc_fts)[0])
-model = model.fit(mfcc_fts, [np.shape(mfcc_fts)[0]])
+low_score = 1e9
+low_score_index = -1
 
-mfcc_test = get_mfcc_features(data[len(data)//2:], samplerate)
-print(model.score(np.asarray(mfcc_test).T))
-file = "../wavs/pop/pop.00007.wav"
-data, samplerate = sf.read(file)
-mfcc_test = get_mfcc_features(data[len(data)//2:], samplerate)
-print(model.score(np.asarray(mfcc_test).T))
-file = "../wavs/pop/pop.00005.wav"
-data, samplerate = sf.read(file)
-mfcc_test = get_mfcc_features(data[len(data)//2:], samplerate)
-print(model.score(np.asarray(mfcc_test).T))
-file = "../wavs/pop/pop.00013.wav"
-data, samplerate = sf.read(file)
-mfcc_test = get_mfcc_features(data[len(data)//2:], samplerate)
-print(model.score(np.asarray(mfcc_test).T))
-file = "../wavs/pop/pop.00025.wav"
-data, samplerate = sf.read(file)
-mfcc_test = get_mfcc_features(data[len(data)//2:], samplerate)
-print(model.score(np.asarray(mfcc_test).T))
+X = []
+Y = []
+
+for i in range(0, np.shape(mfccs)[0], WINDOW_STRIDE):
+	if np.shape(mfccs)[0] - WINDOW <= i:
+		break
+	model = hmm.GaussianHMM(n_components=1	)
+	model = model.fit(mfccs[i:i+WINDOW//2], [WINDOW//2])
+	curr_score = model.score(mfccs[i+WINDOW//2:i+WINDOW], [len(mfccs[i+WINDOW//2:i+WINDOW])])
+	print(curr_score, i, window_number_to_sec(i))
+	X.append(window_number_to_sec(i))
+	Y.append(curr_score * -1)
+	if curr_score < low_score:
+		low_score = curr_score
+		low_score_index = i
+
+
+peaks = find_peaks(X, Y, n=2)
+
+import matplotlib.pyplot as plt  
+plt.plot(X, Y)
+for p in peaks:
+	plt.axvline(x=p[0])
+plt.show()
+
+print(low_score, low_score_index)	
